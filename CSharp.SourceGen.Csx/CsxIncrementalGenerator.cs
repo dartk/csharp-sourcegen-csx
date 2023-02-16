3f2af818f9b8cc1f96f80b4ef20855fa7f48b87c
dartk <dartk@users.noreply.github.com>
2023-02-16 12:20:34 +0700

    Replaces static generator with incremental

---
+++ CSharp.SourceGen.Csx/CsxIncrementalGenerator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+
+
+namespace CSharp.SourceGen.Csx;
+
+
+[Generator(LanguageNames.CSharp)]
+public class CsxIncrementalGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var additionalFiles = context.AdditionalTextsProvider
+            .Select(static (file, token) =>
+            {
+                var source = file.GetText(token)?.ToString() ?? string.Empty;
+                return new AdditionalFile(file.Path, source);
+            })
+            .Where(static x => x.IsNotEmpty);
+
+        var csxScripts = additionalFiles.Where(file =>
+            file.FilePath.EndsWith(".csx")
+            && file.FileName[0] != '_'
+            && file.IsNotEmpty);
+
+        // Scripts can depend on other additional files. To call the generator on this files' changes
+        // we need to include them into the cache
+        var csxScriptsWithDependencies = csxScripts.Combine(additionalFiles.Collect())
+            .Select(static (arg, _) =>
+            {
+                var (csx, additionalFiles) = arg;
+                var builder =
+                    ImmutableArray.CreateBuilder<AdditionalFile>(additionalFiles.Length);
+                foreach (var file in additionalFiles)
+                {
+                    if (csx.Text.Contains(file.FileName))
+                    {
+                        builder.Add(file);
+                    }
+                }
+
+                return (csxToExecute: csx, builder.ToImmutable());
+            });
+
+        context.RegisterSourceOutput(csxScriptsWithDependencies, (productionContext, script) =>
+            CompileScript(productionContext, script.csxToExecute.FilePath));
+    }
+
+
+    private static void CompileScript(SourceProductionContext context, string scriptFile)
+    {
+        var source = ScriptRunner.Run(scriptFile, context.ReportDiagnostic,
+            context.CancellationToken);
+        context.AddSource(Path.GetFileName(scriptFile) + ".out", source);
+    }
+
+
+    private readonly record struct AdditionalFile(string FilePath, string Text)
+    {
+        public string FileName => Path.GetFileName(this.FilePath);
+        public bool IsNotEmpty => !string.IsNullOrWhiteSpace(this.Text);
+    }
+}
---
+++ CSharp.SourceGen.Csx/ScriptRunner.cs
@@ -0,0 +1,112 @@
+﻿using System.Diagnostics;
+using System.Text;
+using Microsoft.CodeAnalysis;
+
+
+namespace CSharp.SourceGen.Csx;
+
+
+public static class ScriptRunner
+{
+    public static string Run(string filePath, Action<Diagnostic> reportDiagnostic,
+        CancellationToken token)
+    {
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "dotnet-script.exe",
+                Arguments = Path.GetFileName(filePath),
+                WorkingDirectory = Path.GetDirectoryName(filePath),
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                StandardOutputEncoding = Encoding.UTF8,
+                RedirectStandardError = true
+            }
+        };
+
+        process.Start();
+
+        var sourceBuilder = BeginReadOutput(process);
+        var errors = BeginReadErrors(process);
+
+        while (!process.WaitForExit(50))
+        {
+            if (token.IsCancellationRequested)
+            {
+                process.Kill();
+                process.WaitForExit(5000);
+                token.ThrowIfCancellationRequested();
+            }
+        }
+
+        if (process.ExitCode == 0)
+        {
+            foreach (var error in errors)
+            {
+                reportDiagnostic(Diagnostic.Create(ScriptWarning, Location.None, error));
+            }
+        }
+        else
+        {
+            foreach (var error in errors)
+            {
+                reportDiagnostic(Diagnostic.Create(ScriptError, Location.None, error));
+            }
+        }
+
+        return sourceBuilder.ToString();
+
+
+        static StringBuilder BeginReadOutput(Process process)
+        {
+            var output = new StringBuilder();
+
+            process.OutputDataReceived += (_, args) => output.AppendLine(args.Data);
+            process.BeginOutputReadLine();
+
+            return output;
+        }
+
+
+        static List<string> BeginReadErrors(Process process)
+        {
+            var errors = new List<string>();
+
+            process.ErrorDataReceived += (_, args) =>
+            {
+                var line = args.Data;
+                if (!string.IsNullOrEmpty(line))
+                {
+                    errors.Add(line);
+                }
+            };
+            process.BeginErrorReadLine();
+
+            return errors;
+        }
+    }
+
+
+    private static readonly DiagnosticDescriptor ScriptError = new(
+        id: $"{DiagnosticIdPrefix}001",
+        title: "C# script threw exception",
+        messageFormat: "{0}",
+        category: DiagnosticCategory,
+        DiagnosticSeverity.Error,
+        isEnabledByDefault: true);
+
+
+    private static readonly DiagnosticDescriptor ScriptWarning = new(
+        id: $"{DiagnosticIdPrefix}002",
+        title: "C# script produced warning",
+        messageFormat: "{0}",
+        category: DiagnosticCategory,
+        DiagnosticSeverity.Warning,
+        isEnabledByDefault: true);
+
+
+    private const string DiagnosticIdPrefix = "SourceGen.Csx";
+    private const string DiagnosticCategory = "CSharp.SourceGen.Csx";
+}
